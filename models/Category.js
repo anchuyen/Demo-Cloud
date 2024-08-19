a443a79829dc12829159956bbeccaab412d13377
An Chuyen <buianhg123@gmail.com>
2024-08-19 10:34:15 +0700

    Toy

---
+++ models/Category.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+
+// Định nghĩa schema cho loại sản phẩm
+const categorySchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String },
+  createdAt: { type: Date, default: Date.now },
+  img: { type: String },
+  updatedAt: { type: Date, default: Date.now }
+});
+
+// Tạo model từ schema
+const Category = mongoose.model('Category', categorySchema);
+
+module.exports = Category;
